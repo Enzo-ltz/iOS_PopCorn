d3522d5b0fb01af601b5400cad42cf2a3a6cbe54
Enzo <vmenzo@Enzos-MacBook-Pro.local>
2021-04-26 01:53:07 -0700

    :sparkles: Able to open youtube links

--- PopCorn/ViewController.swift
+++ PopCorn/ViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var filmLabel1: UILabel!
-    @IBOutlet weak var filmLabel2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,11 +16,11 @@ class ViewController: UIViewController {
     }
     
     private func setupViews() {
-        filmLabel1.layer.borderColor = CGColor.init(red: 109, green: 242, blue: 211, alpha: 1)
-        filmLabel1.layer.backgroundColor = CGColor.init(red: 109, green: 242, blue: 211, alpha: 1)
-        filmLabel1.layer.borderWidth = 1.5
-        filmLabel1.layer.cornerRadius = 15
     }
 
+    @IBAction func PlayButton(_ sender: UIButton) {
+        print("In Button")
+        UIApplication.shared.open(URL(string: "https://www.youtube.com/watch?v=hCKPvMT1E1I")!)
+    }
     
 }
