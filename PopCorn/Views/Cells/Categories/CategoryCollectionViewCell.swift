ba99c13b25ef4f65acfcac9c2e5fe2720881e8a5
Enzo <vmenzo@Enzos-MacBook-Pro.local>
2021-04-27 03:09:32 -0700

    :sparkles: Navigation between categories -> list -> detail works, collection view available

---
+++ PopCorn/Views/Cells/Categories/CategoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewCell.swift
+//  PopCorn
+//
+//  Created by Enzo on 27/04/2021.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ PopCorn/Views/Cells/Categories/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  PopCorn
+//
+//  Created by Enzo on 27/04/2021.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
